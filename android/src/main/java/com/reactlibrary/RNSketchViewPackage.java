94cf2ec41f336c7234adeffb62cd7f17f83a11ac
Andy <flyandi@yahoo.com>
2017-08-27 00:01:12 -0700

    Upgrade to RN 0.47

--- android/src/main/java/com/reactlibrary/RNSketchViewPackage.java
+++ android/src/main/java/com/reactlibrary/RNSketchViewPackage.java
@@ -17,7 +17,6 @@ public class RNSketchViewPackage implements ReactPackage {
       return Collections.emptyList();
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
