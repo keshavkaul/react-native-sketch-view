931a03f5ea14afad4d6f6b7e9da4cfdada2c2646
Youri van Mill <yourivanmill@gmail.com>
2017-10-12 17:20:36 +0200

    Fix PropTypes is not an object

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { 
   requireNativeComponent, 
   View,
